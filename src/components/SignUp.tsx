6bfcf049a6937e532cb50fd50fd3b6573cd0157d
tatojajo <tatojajanidze@gmail.com>
2023-09-19 13:44:25 +0200

    create community and finished authentication

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -2,22 +2,23 @@ import Link from "next/link"
 import { Icons } from "./Icons"
 import UserAuthForm from "./UserAuthForm"
 
-const SignIn = () => {
+const SignUp = () => {
     return (
         <div className="container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]">
             <div className="flex flex-col space-y-2 text-center">
                 <Icons.logo className="mx-auto h-6 w-6" />
-                <h1 className="text-2xl font-semibold tracking-tight">Welcome Back</h1>
+                <h1 className="text-2xl font-semibold tracking-tight">Sign Up</h1>
                 <p className="text-sm max-w-xs mx-auto">By continuing, you are setting up a Breadit account and agree to our User Argeement and Privacy Policy</p>
             </div>
             <UserAuthForm />
 
             {/* Sign In form */}
 
-            <p className="px-8 text-center text-sm text-zinc-700" >New to Breadit
-                <Link href='/sign-up' className="hover:rtext-zinc-800 text-sm underline underline-offset-4">Sign Up</Link></p>
+            <p className="px-8 text-center text-sm text-zinc-700" >Already a Breadittor
+                <Link href='/sign-in' className="hover:rtext-zinc-800 text-sm underline underline-offset-4">Sign In</Link></p>
         </div>
     )
 }
 
-export default SignIn
+export default SignUp
+
